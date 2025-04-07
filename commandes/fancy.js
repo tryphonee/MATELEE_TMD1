51ae8a445eeeb2ed03da2615e647d5bc3280bb0f
MATELEE <tryphonee3@gmail.com>
2025-04-07 05:32:05 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 Anyway-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('ANYWAY-MD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 matelee md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('MATELEE_TMD1', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
