f5f644e51f5c3619e2c280bc6755bfc66d91bdf6
MATELEE <tryphonee3@gmail.com>
2025-04-07 05:21:57 +0300

    Update General.js

--- commandes/General.js
+++ commandes/General.js
@@ -55,15 +55,15 @@ zokou({ nomCom: "dev", categorie: "General", reaction: "🚘" }, async (dest, zk
     const { ms, mybotpic } = commandeOptions;
 
     const devs = [
-      { nom: "Anyway", numero: "255678892560" },
-      { nom: "᚛Ibrahim᚜", numero: "254710772666" },
-      { nom: "Baraka", numero: "255762190568" },
+      { nom: "*MATELEE*", numero: "255697993596" },
+      { nom: "᚛timnasa᚜", numero: "255784766591" },
+      { nom: "timnasa", numero: "255756469954" },
       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
-    let message = "WELCOME TO ANYWAY-MD-V1 HELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:\n\n";
+    let message = "WELCOME TO MATELEE-TMD1 HELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:MATELEE-TMD\n\n";
     for (const dev of devs) {
-      message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
+      message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}MATELEE-TMD1\n`;
     }
   var lien = mybotpic()
     if (lien.match(/\.(mp4|gif)$/i)) {
@@ -95,7 +95,7 @@ else {
 zokou({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
-  repondre("THANK YOU FOR CHOOSING ANYWAY-MD-V1, HERE ARE OUR SUPPORTIVE LINKS\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[https://whatsapp.com/channel/0029VagWQ255q08VTCRQKP09] \n\n ☉ GROUP LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://chat.whatsapp.com/C7tunMI1UjV3mRcdR9Evgz] \n\n ☉YOUTUBE LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://www.youtube.com/@ibrahimaitech] \n\n\n*Created By Mr anyway") 
-  await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING ANYWAY-MD-V1,MAKE SURE YOU FOLLOW THESE LINKS. `},{quoted :ms})
+  repondre("THANK YOU FOR CHOOSING MATELEE-TMD1, HERE ARE OUR SUPPORTIVE LINKS\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[https://whatsapp.com/channel/0029VajweHxKQuJP6qnjLM31] \n\n ☉ GROUP LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://chat.whatsapp.com/JazGLNBxW5XDVEst3PN4kj] \n\n ☉YOUTUBE LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://youtube.com/@timnasa-tmd?si=pnitcokiKJdhzhMA] \n\n\n*Created By matelee md") 
+  await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING MATELEE-TMD1,MAKE SURE YOU FOLLOW THESE LINKS. `},{quoted :ms})
 
 })
